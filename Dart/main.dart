aa0b0af2d2e9e75526a59567e8b9cdd87dd36562
Junior Lopes <albertinolopes@hotmail.com>
2024-10-07 15:45:20 +0200

    tagit bort överflöd

--- Dart/main.dart
+++
@@ -1,6 +0,0 @@
-import 'logic/set_main.dart';
-
-void main() {
-  final SetMain main = new SetMain();
-  main.setMainPage();
-}
--- Dart/models/person.dart
+++
@@ -1,9 +0,0 @@
-class Person {
-  Person({
-    required this.name,
-    required this.socialSecurityNumber,
-  });
-
-  String name;
-  String socialSecurityNumber;
-}
